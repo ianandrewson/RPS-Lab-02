abeb0258256f61801ca48f3617a0c416b191c9fb
Ian Andrewson <ianandrewson@gmail.com>
2019-10-02 17:43:42 -0700

    finished app skeleton

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 import { checkResult } from 'get-random-throw.js';
 import { getRandomThrow } from 'get-random-throw.js';
 
+//Initialize DOM elements
+    //radio buttons
+    //play button
+    //current result area
+    //result table/list
+
+//Set initial state
+    //wins, losses, draws
+
+
+//Add Event listeners
+    //button click
