d59b3ef2a9fea0ee65c26bfa4ba8b25c14bc20b0
Ian Andrewson <ianandrewson@gmail.com>
2019-10-02 19:30:24 -0700

    finished app.js, all functionality included.

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-import { checkResult } from 'get-random-throw.js';
-import { getRandomThrow } from 'get-random-throw.js';
+import { checkResult } from './get-random-throw.js';
+import { getRandomThrow } from './get-random-throw.js';
 
 //Initialize DOM elements
     //radio buttons
-    //play button
-    //current result area
-    //result table/list
+const playButton = document.getElementById('play-button');
+const resultFieldUserThrow = document.getElementById('user-throw');
+const resultFieldCompThrow = document.getElementById('computer-throw');
+const winnerField = document.getElementById('who-won');
+const resultList = document.getElementById('result-list');
 
 //Set initial state
-    //wins, losses, draws
-
+let wins = 0;
+let loses = 0;
+let draws = 0;
 
 //Add Event listeners
-    //button click
+playButton.addEventListener('click', () => {
+    //play game
+    let userThrow = document.querySelector('input:checked').value;
+    let compThrow = getRandomThrow();
+    let result = checkResult(userThrow, compThrow);
+    //update DOM fields
+    resultFieldUserThrow.textContent = userThrow;
+    resultFieldCompThrow.textContent = compThrow;
+    winnerField.textContent = displayGameResult(result);
+
+
+    //update result list
+    //generate to LI for OL
+    let newListItem = document.createElement('li');
+    let textForNewListItem = document.createTextNode(result);
+    newListItem.appendChild(textForNewListItem);
+    resultList.append(newListItem);
+});
+
+const displayGameResult = result => {
+    if (result === 'win') {
+        return 'You won!';
+    } else if (result === 'lose') {
+        return 'You lost.';
+    } else if (result === 'draw') {
+        return 'The game was a draw.';
+    }
+};
